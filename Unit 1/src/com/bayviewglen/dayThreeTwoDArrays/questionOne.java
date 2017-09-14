ec4ee0d89ad4490b522d038517f5ce03e797be09
cmei <cmei@cmeisp3.bayviewglen.ca>
2017-09-14 08:34:28 -0400

    Working on Q1

--- Unit 1/src/com/bayviewglen/dayThreeTwoDArrays/questionOne.java
+++ Unit 1/src/com/bayviewglen/dayThreeTwoDArrays/questionOne.java
@@ -1,10 +1,46 @@
 package com.bayviewglen.dayThreeTwoDArrays;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class questionOne {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int[][] sudoku = new int[9][9];
+		
+		readingSudoku(sudoku);
 
+		// checking
+		for (int[] arr : sudoku) {
+			for (int i : arr) {
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+		
+		checkingRow(sudoku);
+		
+	}
+
+	private static void checkingRow(int[][] sudoku) {
+		// rows 
+		
+	}
+
+	private static void readingSudoku(int[][] sudoku) {
+		try {
+			Scanner input = new Scanner(new File("data/questionOne.dat"));
+			for (int r=0; r<sudoku.length; r++) {
+				for (int c=0; c<sudoku[r].length; c++) {
+					sudoku[r][c] = input.nextInt();
+				}
+			}
+		} catch (FileNotFoundException e){
+			e.printStackTrace();
+		}
+		
 	}
 
 }
