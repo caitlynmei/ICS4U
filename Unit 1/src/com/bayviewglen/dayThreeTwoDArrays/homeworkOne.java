94669526cb94c0832c3cf5ddf9ec054af3ff5bb9
cmei <cmei@cmeisp3.bayviewglen.ca>
2017-09-13 22:36:21 -0400

    ...

--- Unit 1/src/com/bayviewglen/dayThreeTwoDArrays/homeworkOne.java
+++ Unit 1/src/com/bayviewglen/dayThreeTwoDArrays/homeworkOne.java
@@ -1,9 +1,12 @@
 package com.bayviewglen.dayThreeTwoDArrays;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
+
+import com.bayviewglen.dayTwoExceptions.BestExceptionEver;
 
 public class homeworkOne {
 
@@ -11,20 +14,40 @@ public class homeworkOne {
 
 		String[][] questions = new String[8][10];
 		
-		Scanner input = new Scanner("data/homeworkOne.dat");
+		String[] answers = {"D", "B", "D", "C", "C", "D", "A", "E", "A", "D"}; 
 
+		readingQuestions(questions);
 		
-		
-		while (input.hasNext()) {
-			for (int row = 0; row < questions.length; row++) {
-				for (int column = 0; column < questions[0].length; column++) {
-					questions[row][column] = input.next();
-				}
+		for (String[] arr : questions) {
+			for (String i : arr) {
+				System.out.print(i);
 			}
+			System.out.println();
+		}
+		
+		
+		
+		
+		//System.out.println("here: " + questions[3][5]); testing 
+		
+		/*
+		 * 
+		 * 
+		 * 	String[] horses = null;
+		try {
+			Scanner scannerFile = new Scanner(new File("Input/horses.dat"));
+			int numHorses = Integer.parseInt(scannerFile.nextLine()); // take a string and turn it into an int
+			horses = new String[numHorses];
+
+			for (int i = 0; i < numHorses; i++) {
+				horses[i] = scannerFile.nextLine();
+			}
+		} catch (FileNotFoundException e) { // in case file isn't there
+			e.printStackTrace();
 		}
 
-	
-		/*
+		return horses;
+	}
 		FileWriter fw = new FileWriter(new File("data/homeworkOne.dat"));
 		fw.write("\n");
 		fw.write("This is another test...\n");
@@ -38,6 +61,23 @@ public class homeworkOne {
 		}
 		*/
 
+	}
+
+	public static void readingQuestions(String[][] questions) {
+		Scanner input = null;
+		try {
+			input = new Scanner(new File("data/homeworkOne.dat"));
+			
+			for (int row = 0; row < questions.length; row++) {
+				for (int column = 0; column < questions[0].length; column++) {
+					questions[row][column] = input.next();
+				}
+			}		
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+			
 	}
 
 }
