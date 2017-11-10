8d60496f4b04f43f121932efb09837b40af4eb28
cmei <cmei@cmeisp3.bayviewglen.ca>
2017-11-10 11:25:35 -0500

    ... how to use BigInteger?

---
+++ Project Euler/src/com/bayviewglen/firstFifty/oneMultiples.java
@@ -0,0 +1,18 @@
+package com.bayviewglen.firstFifty;
+
+public class oneMultiples {
+
+	public static void main(String[] args) {
+
+		int sum = 0;
+		for (int i = 0; i < 1000; i++) {
+			if (i % 3 == 0 || i % 5 == 0) {
+				sum += i;
+			}
+		}
+
+		System.out.println("The sum of the multiples of 3 and 5 below 1000 is: " + sum);
+
+	}
+
+}
