86d0ff198bea178d17b33802dea5d4da1cc58af5
Caitlyn Mei <cmei@bayviewglen.ca>
2018-01-08 19:13:25 -0500

    still working on diagram

--- ICS4U Workspaces/Kahn's Algorithm/js/script.js
+++ ICS4U Workspaces/Kahn's Algorithm/js/script.js
@@ -1,9 +1,43 @@
 // notes: 
 // context.strokeText("hi", 300, 100); <-- border of text 
-
-
+ 
 var canvas = document.getElementById("myCanvas");
 var context = canvas.getContext("2d");
+
+// first y coordinate for adj list
+var adjY = 150;
+
+// first y coordinate for indegree list
+var inY = 350;
+
+// x and y coordinates of first vertex on top row
+var vTopX = 500; // x coordinate
+var vTopY = 150; // y coordinate 
+
+// x and y coordinates of first vertex on bottom row
+var vBottomX = 570; // x coordinate
+var vBottomY = 350; // y coordinate 
+
+// x coordinates for first label number of vertices 
+var numTopX = vTopX - 7; // top x coordinate
+var numBottomX = vBottomX - 7; // bottom x coordinate
+
+// y coordinates for first label number of vertices 
+var numTopY = vTopY + 9; // top y coordinate
+var numBottomY = vBottomY + 9; // bottom y coordinate
+
+// x and y coordinates for first arrow line on top row
+var arrowTopX = vTopX + 35; // top x coordinate
+var arrowTopY = vTopY; // top y coordinate
+
+// x and y coordinates for first arrow line on bottom row
+var arrowBottomX = vBottomX + 36; // bottom x coordinate
+var arrowBottomY = vBottomY; // bottom y coordinate
+
+// x and y coordinate for first arrow tip
+var arrowTipX = arrowTopX + 55;
+var arrowTipY = vTopY + 10;
+
 
 title();
 adjList();
@@ -12,79 +46,146 @@ drawGraph();
 
 // draw title 
 function title() {
-	context.font = "30px apercu mono";
+	context.font = "27px apercu mono";
 	context.fillStyle = "purple";
 	context.textAlign = "center";
-	context.fillText("Hello World", myCanvas.width/2, 50);
+	context.fillText("Kahn\'s Algorithm", myCanvas.width / 2, 50);
 }
 
 // draw adjacency list of each vertex 
 function adjList() {
-	context.font = "18px Palatino Linotype";
+	context.font = "18px Menlo";
 	context.fillStyle = "black";
 	context.textAlign = "left";
-	context.fillText("Adjacency List of each vertex (outgoing vertices)", 50, 100);
-	context.fillText("0: 1, 4, 6", 60, 130);
-	context.fillText("1: ", 60, 150);
-	context.fillText("2: 6", 60, 170);
-	context.fillText("3: 1, 5, 6", 60, 190);
-	context.fillText("4: 3, 6", 60, 210);	
-	context.fillText("5: ", 60, 230);	
-	context.fillText("6: ", 60, 250);
+	context.fillText("Adjacency List of Each Vertex (Outgoing Vertices)", 50, adjY);
+	context.fillText("0:   1,   4,   6", 60, adjY + 20);
+	context.fillText("1:   ", 60, adjY + 40);
+	context.fillText("2:   6", 60, adjY + 60);
+	context.fillText("3:   1,   5,   6", 60, adjY + 80);
+	context.fillText("4:   3,   6", 60, adjY + 100);	
+	context.fillText("5: ", 60, adjY + 120);	
+	context.fillText("6: ", 60, adjY + 140);
 }
 
 // draw list for indegrees of each vertex
 function indegreeList() {
-	context.font = "18px Palatino Linotype";
+	context.font = "18px Menlo";
 	context.fillStyle = "black";
 	context.textAlign = "left";
-	context.fillText("In-degrees of each vertex", 50, 300);
-	context.fillText("0: 0", 60, 330);
-	context.fillText("1: 2", 60, 350);
-	context.fillText("2: 0", 60, 370);
-	context.fillText("3: 1", 60, 390);
-	context.fillText("4: 1", 60, 410);	
-	context.fillText("5: 1", 60, 430);	
-	context.fillText("6: 4", 60, 450);
+	context.fillText("In-degrees of Each Vertex", 50, inY);
+	context.fillText("0:   0", 60, inY + 20);
+	context.fillText("1:   2", 60, inY + 40);
+	context.fillText("2:   0", 60, inY + 60);
+	context.fillText("3:   1", 60, inY + 80);
+	context.fillText("4:   1", 60, inY + 100);	
+	context.fillText("5:   1", 60, inY + 120);	
+	context.fillText("6:   4", 60, inY + 140);
 }
 
 // draw graph 
 function drawGraph() {
+	drawVertices();
+	drawNumbers();
+	drawArrows();	
+}
+
+// draw vertices
+function drawVertices() {
+	context.fillStyle = "#CCFFFF"; 
+
 	// top row of vertices
 	context.beginPath();
-	context.arc(500, 120, 25, 0, 2*Math.PI, false);
+	context.arc(vTopX, vTopY, 25, 0, 2*Math.PI, false);
+	context.fill();
 	context.stroke();
 	context.closePath();
 	context.beginPath();
-	context.arc(640, 120, 25, 0, 2*Math.PI, false);
+	context.arc(vTopX + 140, vTopY, 25, 0, 2*Math.PI, false);
+	context.fill();
 	context.stroke();
 	context.closePath();
 	context.beginPath();
-	context.arc(780, 120, 25, 0, 2*Math.PI, false);
+	context.arc(vTopX + 280, vTopY, 25, 0, 2*Math.PI, false);
+	context.fill();
 	context.stroke();
 	context.closePath();
 	context.beginPath();
-	context.arc(920, 120, 25, 0, 2*Math.PI, false);
+	context.arc(vTopX + 420, vTopY, 25, 0, 2*Math.PI, false);
+	context.fill();
 	context.stroke();
 	context.closePath();
-	context.beginPath();
 
 	// bottom row of vertices 
 	context.beginPath();
-	context.arc(570, 300, 25, 0, 2*Math.PI, false);
+	context.arc(vBottomX, vBottomY, 25, 0, 2*Math.PI, false);
+	context.fill();
 	context.stroke();
 	context.closePath();
 	context.beginPath();
-	context.arc(710, 300, 25, 0, 2*Math.PI, false);
+	context.arc(vBottomX + 140, vBottomY, 25, 0, 2*Math.PI, false);
+	context.fill();
 	context.stroke();
 	context.closePath();
 	context.beginPath();
-	context.arc(850, 300, 25, 0, 2*Math.PI, false);
+	context.arc(vBottomX + 280, vBottomY, 25, 0, 2*Math.PI, false);
+	context.fill();
 	context.stroke();
 	context.closePath();
 }
 
- 
+// label numbers of each vertex 
+function drawNumbers() {
+	context.font = "30px Menlo";
+	context.fillStyle = "black";
+	context.textAlign = "left";
+
+	// top vertices
+	context.fillText("0", numTopX, numTopY);
+	context.fillText("1", numTopX + 140, numTopY);
+	context.fillText("2", numTopX + 280, numTopY);
+	context.fillText("3", numTopX + 420, numTopY);
+
+	// bottom vertices 
+	context.fillText("4", numBottomX, numBottomY);
+	context.fillText("5", numBottomX + 140, numBottomY);
+	context.fillText("6", numBottomX + 280, numBottomY);
+}
+
+// draw arrows
+function drawArrows() {
+	// top arrows (following vertex labels are for its outgoing vertices)
+	// vertex 0:
+	context.moveTo(arrowTopX, arrowTopY);
+	context.lineTo(arrowTopX + 70, arrowTopY);
+	context.strokeStyle = "#ff0000";
+	context.stroke();
+	context.fillStyle = "#ff0000"; 
+	context.fillText(">", arrowTipX, arrowTipY);
+
+	// vertex 2:
+	context.moveTo(arrowTopX + 252, arrowTopY + 35);
+	context.lineTo(arrowBottomX + 235, arrowBottomY - 35);
+	context.stroke();
+	//context.fillText(">", arrowTipX, arrowTipY); // fix 
+
+	// vertex 3:
+	context.moveTo(arrowTopX + 355, arrowTopY + 22);
+	context.lineTo(arrowBottomX + 120, arrowBottomY - 35);
+	context.stroke();
+	// arrow head!!
+	context.moveTo(arrowTopX + 375, arrowTopY + 33);
+	context.lineTo(arrowBottomX + 255, arrowBottomY - 35);
+	context.stroke();
+	
+	// bottom arrows  
+	// vertex 4:
+	
+
+	// vertex 5: 
+	// vertex 6:
+}
+
+
 
 /*
 context.font = "20px apercu mono";
