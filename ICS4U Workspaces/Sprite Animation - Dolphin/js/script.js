838a5c85429d85942b7c60deb11f66d3fb47b456
Caitlyn Mei <cmei@bayviewglen.ca>
2018-01-23 09:41:46 -0500

    Got Animation to Work

--- ICS4U Workspaces/Sprite Animation - Dolphin/js/script.js
+++ ICS4U Workspaces/Sprite Animation - Dolphin/js/script.js
@@ -1,9 +1,9 @@
-// #1) load sprite sheet into canvas
+
 var canvas = document.getElementById("myCanvas");
 var context = canvas.getContext("2d");
  
 var myImage = new Image();
-myImage.src = "https://www.kirupa.com/stuff/sprites_blue.png";
+myImage.src = "images/Dolphin_Sprite2.png";
 myImage.addEventListener("load", loadImage, false);
  
 function loadImage(e) {
@@ -12,14 +12,15 @@ function loadImage(e) {
 
 // variables 
 var shift = 0; // onto the next sprite image
-var frameWidth = 300;
-var frameHeight = 300;
-var totalFrames = 24;
+var frameWidth = 148.5;
+var frameHeight = 161;
+var totalFrames = 3;
 var currentFrame = 0; // counter 
 
 // animation
 function animate() {
-  context.clearRect(120, 25, 300, 300);
+  context.clearRect(120, 25, 148.5, 161);
+  //context.clearRect(120, 25, 300, 300);
  
   //draw each frame + place them in the middle
   context.drawImage(myImage, shift, 0, frameWidth, frameHeight,
@@ -31,12 +32,12 @@ function animate() {
     Start at the beginning once you've reached the
     end of your sprite!
   */
-  if (currentFrame == totalFrames) {
+  if (currentFrame == (totalFrames - 1)) {
     shift = 0;
     currentFrame = 0;
   }
  
   currentFrame++;
- 
-  requestAnimationFrame(animate);
 }
+
+setInterval(animate, 700);
