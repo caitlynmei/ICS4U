72e269c0cf167053281d51d40f03a60971d3cadb
Caitlyn Mei <cmei@bayviewglen.ca>
2018-01-23 14:59:46 -0500

    Dolphin

--- ICS4U Workspaces/Sprite Animation - Dolphin/js/script.js
+++ ICS4U Workspaces/Sprite Animation - Dolphin/js/script.js
@@ -3,7 +3,7 @@ var canvas = document.getElementById("myCanvas");
 var context = canvas.getContext("2d");
  
 var myImage = new Image();
-myImage.src = "images/Dolphin_Sprite2.png";
+myImage.src = "images/Dolphin_Sprite4New.png";
 myImage.addEventListener("load", loadImage, false);
  
 function loadImage(e) {
@@ -12,17 +12,17 @@ function loadImage(e) {
 
 // variables 
 var shift = 0; // onto the next sprite image
-var frameWidth = 148.5;
-var frameHeight = 161;
-var totalFrames = 3;
+var frameWidth = 196.57;//148.5;
+var frameHeight = 198;//161;
+var totalFrames = 4;
 var currentFrame = 0; // counter 
 
 // animation
 function animate() {
-  context.clearRect(120, 25, 148.5, 161);
+  context.clearRect(120, 25, 196.57, 198);
   //context.clearRect(120, 25, 300, 300);
  
-  //draw each frame + place them in the middle
+  //draw each frame + place them in the 4iddle
   context.drawImage(myImage, shift, 0, frameWidth, frameHeight,
                     120, 25, frameWidth, frameHeight);
  
@@ -32,7 +32,7 @@ function animate() {
     Start at the beginning once you've reached the
     end of your sprite!
   */
-  if (currentFrame == (totalFrames - 1)) {
+  if (currentFrame == (totalFrames-1)) {
     shift = 0;
     currentFrame = 0;
   }
@@ -40,4 +40,4 @@ function animate() {
   currentFrame++;
 }
 
-setInterval(animate, 700);
+setInterval(animate, 600);
