ceb932aea8e967c0c637508e94b2fafae2bef598
Caitlyn Mei <cmei@bayviewglen.ca>
2018-06-07 23:23:08 -0400

    Snake Game: ISP

---
+++ ICS4U Workspaces/MEVN Workspace Testing/components/index.js
@@ -0,0 +1,15 @@
+
+import Vue from "vue"
+import Router from "vue-router"
+import HelloWorld from "@/components/HelloWorld"
+import Posts from "@/components/Posts"
+
+Vue.use(Router)
+
+export default new Router({
+	mode: "history",
+	routes: [
+	{
+		
+	}]
+})
